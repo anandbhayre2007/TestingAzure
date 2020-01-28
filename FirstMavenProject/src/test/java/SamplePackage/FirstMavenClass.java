5a4924be16d9c61a1f07250ca7c4846dd4bf6926
Anand Bhayre <anandbhayre2007@gmail.com>
2020-01-28 15:27:43 +0530

    Commiting second test case

---
+++ FirstMavenProject/src/test/java/SamplePackage/FirstMavenClass.java
@@ -0,0 +1,32 @@
+package samplePackage;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class FirstMavenClass 
+{
+	@Test
+	public void test1()
+	{
+		System.out.println("Testing maven class");
+		
+	}
+	
+	@Test
+	public void test2()
+	{
+		System.setProperty("webdriver.chrome.driver", "D:\\Drivers\\chromedriver");
+		
+		ChromeDriver dr= new ChromeDriver();
+		
+		dr.manage().window().maximize();
+		
+		dr.get("http://newtours.demoaut.com/mercuryreservation.php?osCsid=a903c37c3be8a3133df53f49c38a9a50");
+		
+		dr.findElement(By.name("userName")).sendKeys("mercury");
+		dr.findElement(By.name("password")).sendKeys("mercury");
+		dr.findElement(By.name("login")).click();
+	}
+
+}
