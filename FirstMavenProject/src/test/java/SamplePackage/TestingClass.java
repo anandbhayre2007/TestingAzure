28e1a2c40040256b8186f6e2b406a4aa1d52e356
Anand Bhayre <anandbhayre2007@gmail.com>
2020-01-28 13:06:21 +0530

    commiting first maven project

---
+++ FirstMavenProject/src/test/java/SamplePackage/TestingClass.java
@@ -0,0 +1,12 @@
+package SamplePackage;
+
+import org.testng.annotations.Test;
+
+public class TestingClass {
+
+	@Test
+	public void test()
+	{
+		System.out.println("Testing maven project");
+	}
+}
